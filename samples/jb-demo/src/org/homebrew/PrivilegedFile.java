9de08644210ea6b3dc555991d36a92c9be59e174
John Törnblom <john.tornblom@gmail.com>
2022-07-01 16:07:01 +0200

    jb-demo: add more privileged file operations

--- samples/jb-demo/src/org/homebrew/PrivilegedFile.java
+++ samples/jb-demo/src/org/homebrew/PrivilegedFile.java
@@ -1,6 +1,7 @@
 package org.homebrew;
 
 import java.io.File;
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -9,13 +10,25 @@ public class PrivilegedFile {
     private PrivilegedImpl impl = null;
     
     interface Interface extends Remote {
-	public String[] list() throws RemoteException;
 	public boolean canRead() throws RemoteException;
 	public boolean canWrite() throws RemoteException;
 	public boolean exists() throws RemoteException;
 	public boolean isDirectory() throws RemoteException;
 	public boolean isFile() throws RemoteException;
+	public boolean isHidden() throws RemoteException;
+	public long lastModified() throws RemoteException;
 	public long length() throws RemoteException;
+	public boolean createNewFile() throws IOException, RemoteException;
+	public boolean delete() throws RemoteException;
+	public void deleteOnExit() throws RemoteException;
+	public String[] list() throws RemoteException;
+	public boolean renameTo(File file) throws RemoteException;
+	public boolean setWritable(boolean writable, boolean ownerOnly) throws RemoteException;
+	public boolean setWritable(boolean readable) throws RemoteException;
+	public boolean setReadable(boolean readable, boolean ownerOnly) throws RemoteException;
+	public boolean setReadable(boolean readable) throws RemoteException;
+	public boolean setExecutable(boolean executable, boolean ownerOnly) throws RemoteException;
+	public boolean setExecutable(boolean executable) throws RemoteException;
     }
     
     class PrivilegedImpl extends File implements Interface {
@@ -35,10 +48,6 @@ public class PrivilegedFile {
     
     public String getPath() {
 	return impl.getPath();
-    }
-    
-    public String[] list()  {
-	return (String[])proxy.invokeMethod(new Object[]{}, "list", "()[Ljava/lang/String;");
     }
     
     public boolean canRead() {
@@ -66,9 +75,78 @@ public class PrivilegedFile {
 	return b.booleanValue();
     }
     
+    public boolean isHidden() {
+	Boolean b = (Boolean)proxy.invokeMethod(new Object[]{}, "isHidden", "()Z");
+	return b.booleanValue();
+    }
+
+    public long lastModified() {
+	Long l = (Long)proxy.invokeMethod(new Object[]{}, "lastModified", "()J");
+	return l.longValue();
+    }
+    
     public long length() {
 	Long l = (Long)proxy.invokeMethod(new Object[]{}, "length", "()J");
 	return l.longValue();
+    }
+
+    public boolean createNewFile() {
+	Boolean b = (Boolean)proxy.invokeMethod(new Object[]{}, "createNewFile", "()Z");
+	return b.booleanValue();
+    }
+
+    public boolean delete() {
+	Boolean b = (Boolean)proxy.invokeMethod(new Object[]{}, "delete", "()Z");
+	return b.booleanValue();
+    }
+
+    public void deleteOnExit() {
+	proxy.invokeMethod(new Object[]{}, "deleteOnExit", "()V");
+    }
+
+    public String[] list()  {
+	return (String[])proxy.invokeMethod(new Object[]{}, "list", "()[Ljava/lang/String;");
+    }
+
+    public boolean renameTo(File file) {
+	Boolean b = (Boolean)proxy.invokeMethod(new Object[]{file}, "renameTo", "(Ljava/io/File;)Z");
+	return b.booleanValue();
+    }
+
+    public boolean setWritable(boolean writable, boolean ownerOnly) {
+	Object[] args = new Object[]{new Boolean(writable), new Boolean(ownerOnly)};
+	Boolean b = (Boolean)proxy.invokeMethod(args, "setWritable", "(ZZ)Z");
+	return b.booleanValue();
+    }
+    
+    public boolean setWritable(boolean readable) {
+	Object[] args = new Object[]{new Boolean(readable)};
+	Boolean b = (Boolean)proxy.invokeMethod(args, "setWritable", "(Z)Z");
+	return b.booleanValue();
+    }
+    
+    public boolean setReadable(boolean readable, boolean ownerOnly) {
+	Object[] args = new Object[]{new Boolean(readable), new Boolean(ownerOnly)};
+	Boolean b = (Boolean)proxy.invokeMethod(args, "setReadable", "(ZZ)Z");
+	return b.booleanValue();
+    }
+    
+    public boolean setReadable(boolean readable) {
+	Object[] args = new Object[]{new Boolean(readable)};
+	Boolean b = (Boolean)proxy.invokeMethod(args, "setReadable", "(Z)Z");
+	return b.booleanValue();
+    }
+
+    public boolean setExecutable(boolean executable, boolean ownerOnly) {
+	Object[] args = new Object[]{new Boolean(executable), new Boolean(ownerOnly)};
+	Boolean b = (Boolean)proxy.invokeMethod(args, "setExecutable", "(ZZ)Z");
+	return b.booleanValue();
+    }
+    
+    public boolean setExecutable(boolean executable) {
+ 	Object[] args = new Object[]{new Boolean(executable)};
+	Boolean b = (Boolean)proxy.invokeMethod(args, "setExecutable", "(Z)Z");
+	return b.booleanValue();
     }
 }
 
