e4d87a8ff40a490548ecdd0331efbcd135445d9e
John Törnblom <john.tornblom@gmail.com>
2024-02-24 12:47:00 +0100

    ps5-payload-loader: rewrite elf loader to fixed a couple of bugs

--- samples/ps5-payload-loader/src/org/homebrew/ElfLoading.java
+++ samples/ps5-payload-loader/src/org/homebrew/ElfLoading.java
@@ -21,6 +21,7 @@ public class ElfLoading {
     private static final int OFF_PHDR_FLAGS  = 0x04;
     private static final int OFF_PHDR_OFFSET = 0x08;
     private static final int OFF_PHDR_VADDR  = 0x10;
+    private static final int OFF_PHDR_FILESZ = 0x20;
     private static final int OFF_PHDR_MEMSZ  = 0x28;
 
     private static final int OFF_SHDR_TYPE   = 0x04;
@@ -90,7 +91,8 @@ public class ElfLoading {
 
     public static void spawnServer(int port) throws Exception {
 	new LoadingServer(port) {
-	    public void runPayload(byte[] bytes, OutputStream os) throws Exception {
+	    public void runPayload(byte[] bytes, OutputStream os)
+		throws Exception {
 		ElfLoading.runElf(bytes, os);
 	    }
 	}.spawn();
@@ -106,9 +108,134 @@ public class ElfLoading {
 	}
     }
 
+    private static long ROUND_PG(long val) {
+	return (val + 0x3FFF) & 0xFFFFC000;
+    }
+
+    private static long TRUNC_PG(long val) {
+	return val & 0xFFFFC000;
+    }
+
+    private static int PFLAGS(int p_flags) {
+	int prot = 0;
+
+	if((p_flags & PF_X) == PF_X) {
+	    prot |= PROT_EXEC;
+	}
+	if((p_flags & PF_W) == PF_W) {
+	    prot |= PROT_WRITE;
+	}
+	if((p_flags & PF_R) == PF_R) {
+	    prot |= PROT_READ;
+	}
+
+	return prot;
+    }
+
+    private static void r_relative(long base_addr, long rela_addr)
+	throws Exception {
+	long r_offset = NativeMemory.getLong(rela_addr + OFF_RELA_OFFSET);
+	long r_addend = NativeMemory.getLong(rela_addr + OFF_RELA_ADDEND);
+
+	NativeMemory.putLong(base_addr + r_offset, base_addr + r_addend);
+    }
+
+    private static void pt_load(long elf_addr, long base_addr, long phdr_addr)
+	throws Exception {
+	long p_offset = NativeMemory.getLong(phdr_addr + OFF_PHDR_OFFSET);
+	long p_vaddr = NativeMemory.getLong(phdr_addr + OFF_PHDR_VADDR);
+	long p_filesz = NativeMemory.getLong(phdr_addr + OFF_PHDR_FILESZ);
+	long p_memsz = NativeMemory.getLong(phdr_addr + OFF_PHDR_MEMSZ);
+
+	if(p_memsz == 0) {
+	    return;
+	}
+
+	long memsz = ROUND_PG(p_memsz);
+	long addr = base_addr + p_vaddr;
+
+	if((addr=libkernel.mmap(addr, memsz,
+				PROT_READ | PROT_WRITE,
+				MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE,
+				-1, 0)) == -1) {
+	    throw new Exception(libcInternal.strerror());
+	}
+
+	if(p_filesz > 0) {
+	    NativeMemory.copyMemory(elf_addr + p_offset, addr, p_filesz);
+	}
+    }
+
+    private static void pt_reload(long base_addr, long phdr_addr)
+	throws Exception {
+	long p_offset = NativeMemory.getLong(phdr_addr + OFF_PHDR_OFFSET);
+	long p_vaddr = NativeMemory.getLong(phdr_addr + OFF_PHDR_VADDR);
+	long p_memsz = NativeMemory.getLong(phdr_addr + OFF_PHDR_MEMSZ);
+	int p_flags = NativeMemory.getInt(phdr_addr + OFF_PHDR_FLAGS);
+
+	long addr = base_addr + p_vaddr;
+	long memsz = ROUND_PG(p_memsz);
+	int prot = PFLAGS(p_flags);
+
+	long ret_addr = NativeMemory.allocateMemory(8);
+	long data = NativeMemory.allocateMemory(memsz);
+
+	int alias_fd = -1;
+	int shm_fd = -1;
+
+	// Backup data
+	NativeMemory.copyMemory(addr, data, memsz);
+
+	try {
+	    // Create shm with executable permissions.
+	    if(libkernel.jitCreateSharedMemory(0, memsz,
+					       prot | PROT_READ | PROT_WRITE,
+					       ret_addr) == 0) {
+		shm_fd = NativeMemory.getInt(ret_addr);
+	    } else {
+		throw new Exception(libcInternal.strerror());
+	    }
+
+	    // Map shm into an executable address space.
+	    if(libkernel.mmap(addr, memsz, prot,
+			      MAP_FIXED | MAP_PRIVATE, shm_fd, 0) == -1) {
+		throw new Exception(libcInternal.strerror());
+	    }
+
+	    // Create an shm alias fd with write permissions.
+	    if(libkernel.jitCreateAliasOfSharedMemory(shm_fd,
+						      PROT_READ | PROT_WRITE,
+						      ret_addr) == 0) {
+		alias_fd = NativeMemory.getInt(ret_addr);
+	    } else {
+		throw new Exception(libcInternal.strerror());
+	    }
+
+	    // Map shm alias into a writable address space.
+	    if((addr=libkernel.mmap(0, memsz, PROT_READ | PROT_WRITE,
+				    MAP_SHARED, alias_fd, 0)) == -1) {
+		throw new Exception(libcInternal.strerror());
+	    }
+
+	    // Resore data
+	    NativeMemory.copyMemory(data, addr, memsz);
+	    libkernel.munmap(addr, memsz);
+	} finally {
+	    // Cleanup resources.
+	    NativeMemory.freeMemory(ret_addr);
+	    NativeMemory.freeMemory(data);
+
+	    if(alias_fd != -1) {
+		libkernel.close(alias_fd);
+	    }
+	    if(shm_fd != -1) {
+		libkernel.close(shm_fd);
+	    }
+	}
+    }
+
     public static void runElf(byte[] elf_bytes, OutputStream os) throws Exception {
 	long elf_addr = 0;
-	long ret_addr = 0;
 
 	long base_addr = -1;
 	long base_size = 0;
@@ -124,7 +251,6 @@ public class ElfLoading {
 	}
 
 	try {
-	    ret_addr = NativeMemory.allocateMemory(8);
 	    elf_addr = NativeMemory.allocateMemory(elf_bytes.length);
 	    for(int i=0; i<elf_bytes.length; i++) {
 		NativeMemory.putByte(elf_addr + i, elf_bytes[i]);
@@ -140,15 +266,10 @@ public class ElfLoading {
 	    // Compute size of virtual memory region.
 	    for(int i=0; i<e_phnum; i++) {
 		long phdr_addr = elf_addr + e_phoff + (i * SIZE_PHDR);
-		int p_type  = NativeMemory.getInt(phdr_addr + OFF_PHDR_TYPE);
 		long p_vaddr = NativeMemory.getLong(phdr_addr + OFF_PHDR_VADDR);
 		long p_memsz = NativeMemory.getLong(phdr_addr + OFF_PHDR_MEMSZ);
 
-		if(p_memsz == 0) {
-		    continue;
-		}
-
-		if(p_vaddr < min_vaddr || min_vaddr < 0) {
+		if(p_vaddr < min_vaddr || min_vaddr == -1) {
 		    min_vaddr = p_vaddr;
 		}
 
@@ -157,8 +278,8 @@ public class ElfLoading {
 		}
 	    }
 
-	    min_vaddr = min_vaddr & 0xFFFFC000;
-	    max_vaddr = (max_vaddr + 0x3FFF) & 0xFFFFC000;
+	    min_vaddr = TRUNC_PG(min_vaddr);
+	    max_vaddr = ROUND_PG(max_vaddr);
 	    base_size = max_vaddr - min_vaddr;
 
 	    int flags = MAP_PRIVATE | MAP_ANONYMOUS;
@@ -177,100 +298,33 @@ public class ElfLoading {
 		throw new Exception(libcInternal.strerror());
 	    }
 
-	    // Commit segments to reserved address space.
+	    // Parse program headers.
 	    for(int i=0; i<e_phnum; i++) {
 		long phdr_addr = elf_addr + e_phoff + (i * SIZE_PHDR);
 		int p_type  = NativeMemory.getInt(phdr_addr + OFF_PHDR_TYPE);
-		int p_flags = NativeMemory.getInt(phdr_addr + OFF_PHDR_FLAGS);
-		long p_offset = NativeMemory.getLong(phdr_addr + OFF_PHDR_OFFSET);
-		long p_vaddr = NativeMemory.getLong(phdr_addr + OFF_PHDR_VADDR);
-		long p_memsz = NativeMemory.getLong(phdr_addr + OFF_PHDR_MEMSZ);
 
-		long aligned_memsz = (p_memsz + 0x3FFF) & 0xFFFFC000;
-		long addr = base_addr + p_vaddr;
-
-		if(p_memsz == 0) {
-		    continue;
-		}
-
-		if((p_flags & PF_X) == PF_X) {
-		    int alias_fd = -1;
-		    int shm_fd = -1;
-
-		    // Create shm with executable permissions.
-		    if(libkernel.jitCreateSharedMemory(0, aligned_memsz,
-						       PROT_READ | PROT_WRITE | PROT_EXEC,
-						       ret_addr) != 0) {
-			throw new Exception(libcInternal.strerror());
-		    }
-		    if((shm_fd = NativeMemory.getInt(ret_addr)) == 0) {
-			throw new Exception(libcInternal.strerror());
-		    }
-
-		    // Map shm into an executable address space.
-		    if((addr = libkernel.mmap(addr, aligned_memsz,
-					      PROT_READ | PROT_EXEC,
-					      MAP_FIXED | MAP_SHARED,
-					      shm_fd, 0)) == -1) {
-			throw new Exception(libcInternal.strerror());
-		    }
-
-		    // Create an shm alias fd with writable permissions.
-		    if(libkernel.jitCreateAliasOfSharedMemory(shm_fd, PROT_WRITE,
-							      ret_addr) != 0) {
-			throw new Exception(libcInternal.strerror());
-		    }
-		    if((alias_fd = NativeMemory.getInt(ret_addr)) == 0) {
-			throw new Exception(libcInternal.strerror());
-		    }
-
-		    // Map shm alias into a writable address space.
-		    if((addr = libkernel.mmap(0, aligned_memsz,
-					      PROT_WRITE, MAP_SHARED,
-					      alias_fd, 0)) == -1) {
-			throw new Exception(libcInternal.strerror());
-		    }
-
-		    // Copy segment data.
-		    NativeMemory.copyMemory(elf_addr + p_offset, addr, p_memsz);
-
-		    // Cleanup resources.
-		    libkernel.munmap(addr, aligned_memsz);
-		    libkernel.close(alias_fd);
-		    libkernel.close(shm_fd);
-		} else {
-		    // Map segment into a writable address space.
-		    if((addr = libkernel.mmap(addr, aligned_memsz,
-					      PROT_WRITE,
-					      MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE,
-					      -1, 0)) == -1) {
-			throw new Exception(libcInternal.strerror());
-		    }
-		    // Copy in segment data
-		    NativeMemory.copyMemory(elf_addr + p_offset, addr, p_memsz);
+		if(p_type == PT_LOAD) {
+		    pt_load(elf_addr, base_addr, phdr_addr);
 		}
 	    }
 
-	    // Relocate positional independent symbols.
+	    // Apply relocations.
 	    for(int i=0; i<e_shnum; i++) {
 		long shdr_addr = elf_addr + e_shoff + (i * SIZE_SHDR);
                 int sh_type = NativeMemory.getInt(shdr_addr + OFF_SHDR_TYPE);
-		long sh_size = NativeMemory.getLong(shdr_addr + OFF_SHDR_SIZE);
-		long sh_offset = NativeMemory.getLong(shdr_addr + OFF_SHDR_OFFSET);
 
                 if (sh_type != SHT_RELA) {
 		    continue;
 		}
 
+		long sh_offset = NativeMemory.getLong(shdr_addr + OFF_SHDR_OFFSET);
+		long sh_size = NativeMemory.getLong(shdr_addr + OFF_SHDR_SIZE);
+
 		for (int j=0; j<sh_size/SIZE_RELA; j++) {
 		    long rela_addr = elf_addr + sh_offset + (SIZE_RELA * j);
-		    long r_offset = NativeMemory.getLong(rela_addr + OFF_RELA_OFFSET);
 		    long r_info = NativeMemory.getLong(rela_addr + OFF_RELA_INFO);
-		    long r_addend = NativeMemory.getLong(rela_addr + OFF_RELA_ADDEND);
-
 		    if(r_info == R_X86_64_RELATIVE) {
-			NativeMemory.putLong(base_addr + r_offset,
-					     base_addr + r_addend);
+			r_relative(base_addr, rela_addr);
 		    }
 		}
 	    }
@@ -278,30 +332,24 @@ public class ElfLoading {
 	    // Set protection bits on mapped segments.
 	    for(int i=0; i<e_phnum; i++) {
 		long phdr_addr = elf_addr + e_phoff + (i * SIZE_PHDR);
-		int p_type  = NativeMemory.getInt(phdr_addr + OFF_PHDR_TYPE);
-		int p_flags = NativeMemory.getInt(phdr_addr + OFF_PHDR_FLAGS);
-		long p_vaddr = NativeMemory.getLong(phdr_addr + OFF_PHDR_VADDR);
 		long p_memsz = NativeMemory.getLong(phdr_addr + OFF_PHDR_MEMSZ);
+		long p_vaddr = NativeMemory.getLong(phdr_addr + OFF_PHDR_VADDR);
+		int p_type = NativeMemory.getInt(phdr_addr + OFF_PHDR_TYPE);
+		int p_flags = NativeMemory.getInt(phdr_addr + OFF_PHDR_FLAGS);
 
-		long aligned_memsz = (p_memsz + 0x3FFF) & 0xFFFFC000;
-		long addr = base_addr + p_vaddr;
-
-		if(p_memsz == 0) {
+		if(p_type != PT_LOAD || p_memsz == 0) {
 		    continue;
 		}
 
-		int prot = 0;
 		if((p_flags & PF_X) == PF_X) {
-		    prot |= PROT_EXEC;
-		}
-		if((p_flags & PF_W) == PF_W) {
-		    prot |= PROT_WRITE;
-		}
-		if((p_flags & PF_R) == PF_R) {
-		    prot |= PROT_READ;
+		    pt_reload(base_addr, phdr_addr);
+		    continue;
 		}
 
-		if(libkernel.mprotect(addr, aligned_memsz, prot) != 0) {
+		long addr = base_addr + p_vaddr;
+		long memsz = ROUND_PG(p_memsz);
+		int prot = PFLAGS(p_flags);
+		if(libkernel.mprotect(addr, memsz, prot) != 0) {
 		    throw new Exception(libcInternal.strerror());
 		}
 	    }
@@ -345,9 +393,6 @@ public class ElfLoading {
 	} finally {
 	    if(elf_addr != 0) {
 		NativeMemory.freeMemory(elf_addr);
-	    }
-	    if(ret_addr != 0) {
-		NativeMemory.freeMemory(ret_addr);
 	    }
 	    if(base_addr != -1) {
 		libkernel.munmap(base_addr, base_size);
